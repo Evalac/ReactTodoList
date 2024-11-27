489f57f3e708d114bee91b0309e6626e55dbb45f
Evalac <maslenokprograming.m@gmail.com>
2024-11-27 16:18:05 +0200

    todo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import { TodoList } from '../components/TodoList';
+import initialTodos from '../todos.json';
+
+export class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+  render() {
+    const { todos } = this.state;
+
+    const allTodos = todos.length;
+    const toDoComleted = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0
+    );
+
+    return (
+      <>
+        <div>
+          <p>Кількість ToDo: {allTodos}</p>
+          <p>Кількість виконаних ToDo: {toDoComleted}</p>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </>
+    );
+  }
+}
