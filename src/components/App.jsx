dc56539cc338133454527db345ccc79a73c3cf27
Evalac <maslenokprograming.m@gmail.com>
2024-12-04 18:00:46 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,22 @@ export class App extends Component {
     todos: initialTodos,
   };
 
+  test() {
+    console.log(this.state.todos.map(todos => todos.completed));
+  }
+
   deleteTodo = todoId => {
     this.setState(prevState => ({
       todos: prevState.todos.filter(todo => todo.id !== todoId),
     }));
   };
   render() {
+    this.test();
+
     const { todos } = this.state;
 
     const allTodos = todos.length;
+
     const toDoComleted = todos.reduce(
       (total, todo) => (todo.completed ? total + 1 : total),
       0
